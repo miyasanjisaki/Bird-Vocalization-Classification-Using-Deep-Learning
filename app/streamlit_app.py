89157c38b4f0260639523165b23f5d15a0b46300
miyasanjisaki <429230857@qq.com>
2025-10-28 12:46:21 +0800

    Bird check

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -77,7 +77,6 @@ def main():
         events = predict_audio(tmp_path, model=model, idx2label=idx2label)
     finally:
         os.unlink(tmp_path)
-    st.write("调试输出 events:", events)
 
     events_df = pd.DataFrame(events)
     events_df.insert(0, "audio_file", uploaded_file.name)
